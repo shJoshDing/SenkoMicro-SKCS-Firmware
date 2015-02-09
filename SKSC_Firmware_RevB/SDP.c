6674c1d161e841624ee4d0809ee0c145d75a50cc
shJoshDing <joshdingone@gmail.com>
2015-02-09 11:01:40 +0800

    Add HDR connector signal definition file
    @shJoshDing

--- SKSC_Firmware_RevB/SDP.c
+++ SKSC_Firmware_RevB/SDP.c
@@ -41,7 +41,7 @@ static SDP_VERSION version = {	0x0301,         // major rev
 								527,       // blackfin software rev		
 								__DATE__,  // date of build
 								__TIME__,  // time of build
-								0xf511,         // reserved
+								0xf515,         // reserved
 								0 };       // flags
 static void mainInit(void);
 void closeMainInit(void);
